c417d41a46c08fa111fbeb480201b18ecb189eb0
Aaron Glick <league@WorthyMini-5.attlocal.net>
2018-09-15 14:23:37 -0700

    MoreArrayFun

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -15,12 +15,18 @@ public class MoreArrayFun {
 		words[4] = "english";
 		print(words);
 		printReverse(words);
+		printOther(words);
+		printRandom(words);
 	}
 	
 	
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
 	public static void print(String[] multiWord) {
-		System.out.println(multiWord);
+		System.out.println("Order:");
+		for (int i = 0; i < multiWord.length; i++) {
+			System.out.println(multiWord[i]);
+		}
+		
 		
 	}
 	
@@ -28,6 +34,7 @@ public class MoreArrayFun {
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
 	public static void printReverse(String[] ReverseWord) {
+		System.out.println("Reverse Order:");
 		for (int i = ReverseWord.length-1; i > -1; i--) {
 			System.out.println(ReverseWord[i]);
 		}
@@ -37,13 +44,21 @@ public class MoreArrayFun {
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
 	public static void printOther(String[] OtherWord) {
+		System.out.println("Every other word:");
 		for (int i = 0; i < OtherWord.length; i+=2) {
-			System.out.println(i);
+			System.out.println(OtherWord[i]);
 		}
 	}
 	
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
-	
+	public static void printRandom(String[] RandomWord) {
+		Random rand = new Random();
+		
+		for (int i = 0; i < RandomWord.length; i++) {
+			int random = rand.nextInt(4);
+			System.out.println(RandomWord[random]);
+		}
+	}
 	
 }
